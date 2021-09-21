1d348cf57479b51b472dd0be5b0a67eb9a3a1df6
makoto07karin <no.one.best.way07@gmail.com>
2021-09-21 17:56:37 +0900

    新規登録ができる

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users #これはこれだけでdeviseと対応しているところなので、このまま放置OK 9/21
   get 'prototypes/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "prototypes#index"

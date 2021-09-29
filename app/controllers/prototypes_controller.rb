e23a13a6b3226e0a703de92e17ea699279ebff9e
makoto07karin <no.one.best.way07@gmail.com>
2021-09-29 10:34:16 +0900

    destroy commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,56 +4,46 @@ class PrototypesController < ApplicationController
 #1 authenticate_user!の効果が:new, :edit, :destroyだけに出るように記述したがこれが間違い？
 
   def index
-    @prototypes = Prototype.all #Prototypesで記述するのがおすすめ
-                                #今回はここはallでOK！理由は＿prototypeのimage_tag(prototype.image）でimageを選択しているから
-     
-      #@prototypesの記述はindexのeachでprototypesテーブルから適したカラムを呼び出すため
-      
-      #0923発展実装の時は、ここに何かを紐付けしないとトップページで表示されない？
-      #↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-      #ここで定義されているものが各viewでも使える
+    @prototypes = Prototype.all
   end
+    #Prototypesで記述するのがおすすめ
+    #今回はここはallでOK理由は、全てのレコードをallでここに持ってきたいから
+    #なぜここに持ってきたいのかというとviewである＿prototypeのimage_tag(prototype.image）で
+    #レコード内の全てのimageカラムを取得したいから
+    #@prototypesの記述は＠をつけることでviewでも使用可能になるから
+    #そこから、indexのeachでprototypesテーブルから適したカラムを呼び出すため
   
-          #0922indexに全てのプロトタイプの情報を代入するとはどういうこと？ここにアクションがつながる様にすること？
-          #情報がdbのことならモデルからAllで持ってくる？
-           #↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-           #設定次第、今回は上記の様に_prototypeで選択しているからallで大丈夫！
   def new
     @prototype = Prototype.new
   end
 
   def show #詳細ページ
     @prototype = Prototype.find(params[:id])
-    @comment = Comment.new #こことview /showがつながるなんで？
+    @comment = Comment.new 
     @comments = @prototype.comments
-    #includes(:user)を使用する？N+1 問題が発生しているのか？
   end
-        #Prototypeモデルから、何を持ってきているのか？
-        #今回はレコード一つ分を持ってきている
-        #それを記述しているのがfind(params[:id])
-        #これがPrototypeのレコード一つ分の情報
-        #なので、idが「１」の時は該当するレコードを１行を持ってくるので
-        #viewではカラムを選んでいる
+    #こことview /showがつながるなんで？
+    #PrototypesControllerのviewの中にshowが記述されているから
+    #Prototypeモデルから、何を持ってきているのか？
+    #@prototype = Prototype.find(params[:id])でPrototypeテーブルのレコードを一つ取得している
+    #それを記述しているのがfind(params[:id])
+    #これがPrototypeのレコード一つ分の情報
+    #なので、idが「１」の時は該当するレコードを１行を持ってくるので
+    #viewではカラムを選んでいる
 
-
-
-
-      #Pathパラメータで送信されるID値で、Prototypeモデルの特定のオブジェクトを取得するように記述
-      #というのは、なんのID値が欲しいのかまたPathパラメータは一意なソース例えばユーザーIDとかなんの値が欲しいのか
-      #.find(params[:id])この形でテーブルからidを選んで表示してくれる今回何度もエラーが出たのは、Urlに引き出したidを入力していなかったらから
-      #showの内容が見たければ、idを入力するただし、viewを繋ぐまで
 
   def create
     
     #保存機能は、createではなくnew・saveを使用するのが基本になる！
-    @prototype = Prototype.new(prototype_params)
+    @prototype = Prototype.new(prototype_params)#prototype_paramsは引数としてプライベートから持ってきている
     if @prototype.save
       redirect_to root_path  #入力されたデータの保存先を指定できていない、わかっていない⇨⇨⇨⇨⇨⇨⇨⇨⇨prototypesテーブルとアクティブテーブルにCとMで制限をかけて保存される
                               #今回0920はまだパスは実装しなくて良いのでroot_pathを使用
                               #ここで入力されたデータをdbに入れたい⇨そのための保存機能
     else
       render :new
-      #今回はnewでOK理由は、コントローラー内のアクションだから！
+      #今回はnewでOK理由は、viewで返したいのはコントローラー内のアクションだから！
+      #なので例えば他のコントローラー内のviewを指定すると別の記述が必要
     end
   end
 
